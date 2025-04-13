523db8ede75847131158c93d74c330e656a2c971
Umar <umarr.k220@gmail.com>
2025-04-13 18:51:19 +0500

    Update server.js

--- Secure Coding & Initial Implementation  (Week 4-6)/backend/server.js
+++ Secure Coding & Initial Implementation  (Week 4-6)/backend/server.js
@@ -1,1 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
+const rateLimit = require('express-rate-limit');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 
+const RequestLog = require('./models/requestLog');
+const User = require('./models/user');
+const BlacklistedIp = require('./models/blacklistedIp');
+const Threat = require('./models/threat');
+
+console.log("Loaded JWT_SECRET:", process.env.JWT_SECRET);
