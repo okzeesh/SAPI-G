dc2e66bcd68fcb3aa8499e729febf0a9f1a5fb81
Umar <umarr.k220@gmail.com>
2025-04-13 18:51:54 +0500

    Update server.js

--- Secure Coding & Initial Implementation  (Week 4-6)/backend/server.js
+++ Secure Coding & Initial Implementation  (Week 4-6)/backend/server.js
@@ -14,3 +14,11 @@ const BlacklistedIp = require('./models/blacklistedIp');
 const Threat = require('./models/threat');
 
 console.log("Loaded JWT_SECRET:", process.env.JWT_SECRET);
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('tiny'));
