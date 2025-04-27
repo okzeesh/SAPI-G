480567f1684e34a4f11887a7f86633d9e512cd3d
Ahmad <ahmed_aaa1988@yahoo.com>
2025-04-28 01:55:01 +0500

    updating the file

--- Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/index.js
+++ Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/index.js
@@ -3,3 +3,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'; // ✅ Import
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
