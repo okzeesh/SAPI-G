555fe38ce87daf4ec55261171e7b6504c1d036b4
Ahmad <ahmed_aaa1988@yahoo.com>
2025-04-28 01:54:41 +0500

    importing the necessary file

---
+++ Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/index.js
@@ -0,0 +1,5 @@
+import './styles/index.css'; // Global styles
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom'; // ✅ Import
