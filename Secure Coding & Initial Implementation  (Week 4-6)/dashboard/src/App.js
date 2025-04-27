181a9059f95f0af72cfc48e329d15de4243a1cdc
Ahmad <ahmed_aaa1988@yahoo.com>
2025-04-28 00:58:48 +0500

    Updating the file

---
+++ Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/App.js
@@ -0,0 +1,7 @@
+// App.js
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+
