2cdd3fe06a1ee7466a32efd2b94787320d5685c1
Umar <umarr.k220@gmail.com>
2025-04-28 22:14:19 +0500

    Update passport.js

--- Secure Coding & Initial Implementation  (Week 4-6)/config/passport.js
+++ Secure Coding & Initial Implementation  (Week 4-6)/config/passport.js
@@ -1,3 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/user");
+
+// Configure Passport
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback"
