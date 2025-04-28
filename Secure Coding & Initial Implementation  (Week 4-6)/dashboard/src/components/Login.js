a9f01fa2945eb1c467cd84148e8865e0081b4aed
AyelaIsrarHaqqani <151562550+AyelaIsrarHaqqani@users.noreply.github.com>
2025-04-28 19:30:15 +0500

    Update Login.js

--- Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Login.js
+++ Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Login.js
@@ -111,6 +111,7 @@ const Login = ({ setIsLoggedIn }) => {
           <div className="security-tag">AI-Powered Threat Detection Active</div>
         </form>
       ) : (
+        <form onSubmit={handleRegister}>
 
           
 
