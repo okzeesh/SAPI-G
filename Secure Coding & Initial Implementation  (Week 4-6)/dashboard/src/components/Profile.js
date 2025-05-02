3d319f38aba236acef2cb0d9b2d72c1eb925040d
Ahmad <ahmed_aaa1988@yahoo.com>
2025-05-03 00:35:07 +0500

    creating a profile function

--- Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Profile.js
+++ Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Profile.js
@@ -2,3 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from axios;
 import '../styles/Profile.css';
+
+const Profile = () => {
+    const [user, setUser] = useState(null);
+  
