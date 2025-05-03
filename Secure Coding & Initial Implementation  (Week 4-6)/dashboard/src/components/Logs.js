9503ad17206f18631430e84679398ede1846f4fc
Muhammad Zeeshan Khan <muhammadzeeshan123345@gmail.com>
2025-05-03 16:33:17 +0500

    Update Logs.js

--- Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Logs.js
+++ Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Logs.js
@@ -2,3 +2,4 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "../styles/Profile.css";
+import { API_URL } from "../config";
