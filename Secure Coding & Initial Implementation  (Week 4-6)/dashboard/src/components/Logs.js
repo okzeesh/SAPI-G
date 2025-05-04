f4e51ec43a46f2b1c9dec582c3df936d101599bc
Muhammad Zeeshan Khan <muhammadzeeshan123345@gmail.com>
2025-05-04 23:14:23 +0500

    Update Logs.js

--- Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Logs.js
+++ Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Logs.js
@@ -126,3 +126,5 @@ const exportAsPDF = () => {
               fontWeight: 600,
               fontSize: "1rem",
               cursor: "pointer",
+              transition: "transform 0.2s, box-shadow 0.2s",
+              boxShadow: "0 4px 8px rgba(0, 242, 195, 0.3)",
