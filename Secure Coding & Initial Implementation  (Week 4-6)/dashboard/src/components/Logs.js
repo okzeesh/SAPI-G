6af5f5458ef449bc78ee379d6221a2d63a6355b1
Muhammad Zeeshan Khan <muhammadzeeshan123345@gmail.com>
2025-05-04 23:13:53 +0500

    Update Logs.js

--- Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Logs.js
+++ Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Logs.js
@@ -120,3 +120,5 @@ const exportAsPDF = () => {
             style={{
               padding: "0.7rem 1.5rem",
               borderRadius: "8px",
+              background: "linear-gradient(90deg, #00f2c3, #81ecec)",
+              color: "#fff",
