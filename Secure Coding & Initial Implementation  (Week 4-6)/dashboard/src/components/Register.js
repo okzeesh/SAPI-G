c047c1a9dbb97d057d2e472aefcab5f3d1eeee09
Umar <umarr.k220@gmail.com>
2025-05-07 13:57:21 +0500

    Update Register.js

--- Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Register.js
+++ Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Register.js
@@ -3,3 +3,9 @@ import axios from 'axios';
 import '../styles/Register.css';
 import { useNavigate } from 'react-router-dom';
 import { API_URL } from '../config';
+
+const Register = ({ setIsLoggedIn }) => {
+    const [registerData, setRegisterData] = useState({
+      firstName: '',
+      lastName: '',
+      username: '',
