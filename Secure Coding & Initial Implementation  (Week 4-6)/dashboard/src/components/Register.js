72038a8fb8a643c1789c114652b6610d6129468f
Umar <umarr.k220@gmail.com>
2025-05-07 13:57:12 +0500

    Create Register.js

---
+++ Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Register.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../styles/Register.css';
+import { useNavigate } from 'react-router-dom';
+import { API_URL } from '../config';
