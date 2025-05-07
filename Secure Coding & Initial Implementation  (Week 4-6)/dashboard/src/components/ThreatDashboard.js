4c047c7f3f247552cee1098014c1b34d427ea0d8
Muhammad Zeeshan Khan <muhammadzeeshan123345@gmail.com>
2025-05-07 23:43:47 +0500

    Update ThreatDashboard.js

--- Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/ThreatDashboard.js
+++ Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/ThreatDashboard.js
@@ -1,2 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid, Area } from 'recharts';
+import { API_URL } from '../config';
+
+const ThreatDashboard = () => {
