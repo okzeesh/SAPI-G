d78d7ad04d50a47e8076adb2af4e02fdd79e52b3
Ahmad <ahmed_aaa1988@yahoo.com>
2025-05-09 00:45:22 +0500

    Update train_model.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
@@ -119,3 +119,5 @@ def train_model(X_train, y_train, X_test, y_test):
     
     # Define parameter grid for tuning
     param_grid = {
+        'n_estimators': [100, 200],
+        'max_depth': [4, 6],
