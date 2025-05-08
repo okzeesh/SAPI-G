daf047f8cce84580e2dbeaed7649297811258c64
AyelaIsrarHaqqani <ayelaisrarhaqqani@gmail.com>
2025-05-09 02:12:47 +0500

    Update generate_best_raw_attack_dataset.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/generate_best_raw_attack_dataset.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/generate_best_raw_attack_dataset.py
@@ -17,5 +17,5 @@ sqli_payloads = [
 ]
 xss_payloads = [
         '<script>alert(1)</script>', '<img src=x onerror=alert(1)>', '<svg/onload=alert(1)>',
-    '<body onload=alert(1)>', '<iframe src=javascript:alert(1)>', '<a href=javascript:alert(1)>Click</a>',
+        '<body onload=alert(1)>', '<iframe src=javascript:alert(1)>', '<a href=javascript:alert(1)>Click</a>',
     '<div onmouseover=alert(1)>XSS</div>', '<input onfocus=alert(1)>'
