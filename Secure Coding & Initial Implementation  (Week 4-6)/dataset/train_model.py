752b447801e3ba4b1142fa9d4aa2641daf375315
Ahmad <ahmed_aaa1988@yahoo.com>
2025-05-09 00:45:00 +0500

    Update train_model.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
@@ -114,4 +114,5 @@ def train_model(X_train, y_train, X_test, y_test):
     model = RandomForestClassifier(
         random_state=42,
         class_weight='balanced',
-        
+        n_jobs=-1
+    )
