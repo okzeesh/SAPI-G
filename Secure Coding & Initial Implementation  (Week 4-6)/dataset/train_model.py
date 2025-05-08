d87f605af1c1a5872807d3e933f693ae4f366443
Ahmad <ahmed_aaa1988@yahoo.com>
2025-05-09 00:39:58 +0500

    Update train_model.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
@@ -85,4 +85,7 @@ def preprocess_data(df):
     label_encoder = LabelEncoder()
     y_encoded = label_encoder.fit_transform(y)
     
-    
+    # Split the data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y_encoded, test_size=0.2, random_state=42, stratify=y_encoded
+    )
