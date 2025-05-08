ed8c82a6dfac9fad8cd6016b294a1c4d40f66490
Ahmad <ahmed_aaa1988@yahoo.com>
2025-05-09 00:30:59 +0500

    Update train_model.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
@@ -2,3 +2,5 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import classification_report, confusion_matrix
