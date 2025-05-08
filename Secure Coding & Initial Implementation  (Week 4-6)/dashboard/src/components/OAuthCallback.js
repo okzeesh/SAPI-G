653613a10bc3c45cfe1eeb72e060ce9ffc36e492
Umar <umarr.k220@gmail.com>
2025-05-08 23:37:54 +0500

    Update OAuthCallback.js

--- Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/OAuthCallback.js
+++ Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/OAuthCallback.js
@@ -1,2 +1,6 @@
 import { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+
+function OAuthCallback({ setIsLoggedIn }) {
+    const navigate = useNavigate();
+    const location = useLocation();
