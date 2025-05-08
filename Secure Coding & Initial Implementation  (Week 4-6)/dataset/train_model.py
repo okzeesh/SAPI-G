c00b624602c8a7c9f3cac2338a59ae18575e992f
Ahmad <ahmed_aaa1988@yahoo.com>
2025-05-09 00:47:43 +0500

    Update train_model.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
@@ -145,4 +145,5 @@ def train_model(X_train, y_train, X_test, y_test):
          # Fit the model with sample weights
         grid_search.fit(X_train, y_train, sample_weight=sample_weights)
         
-        
+        print("\nBest parameters found:")
+        print(grid_search.best_params_)
