320867f4153f340df2261baf74ac3abb2a4d55aa
Ahmad <ahmed_aaa1988@yahoo.com>
2025-05-09 00:50:07 +0500

    Update train_model.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
@@ -170,4 +170,7 @@ def train_model(X_train, y_train, X_test, y_test):
     print("\nClassification Report:")
     report = classification_report(
         y_test, y_pred,
+        target_names=label_encoder.classes_,
+        digits=4
+    )
     
