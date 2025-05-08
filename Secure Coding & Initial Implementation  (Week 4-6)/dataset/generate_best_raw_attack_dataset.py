509897d419103aca1f0f35961b3655ad18a19c0c
AyelaIsrarHaqqani <ayelaisrarhaqqani@gmail.com>
2025-05-09 02:09:36 +0500

    Update generate_best_raw_attack_dataset.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/generate_best_raw_attack_dataset.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/generate_best_raw_attack_dataset.py
@@ -7,4 +7,4 @@ import string
 benign_passwords = [
         'myDogRex2024', 'BlueSky!@#', 'CoffeeLover99', 'SecurePass!2024', 'Bookworm_42',
         'Travel4Fun', 'GuitarHero2023', 'Sunset$Beach', 'WinterIsComing', 'HelloWorld2024',
-    'MapleTreeHouse', 'SoccerFan_88', 'JazzLover2022', 'MountainHike$', 'OceanView!2023',
+        'MapleTreeHouse', 'SoccerFan_88', 'JazzLover2022', 'MountainHike$', 'OceanView!2023',
