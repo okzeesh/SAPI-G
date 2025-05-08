84dc3289e6cd03caa4f4f986ee590735c0a5ebb3
AyelaIsrarHaqqani <ayelaisrarhaqqani@gmail.com>
2025-05-09 02:30:08 +0500

    Update generate_best_raw_attack_dataset.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/generate_best_raw_attack_dataset.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/generate_best_raw_attack_dataset.py
@@ -112,3 +112,4 @@ for _ in range(benign_count):
     ip = generate_random_ip()
     timestamp = random_timestamp()
     endpoint = random.choice(endpoints)
+        rows.append({'email': email, 'password': password, 'user_agent': user_agent, 'ip': ip, 'timestamp': timestamp, 'endpoint': endpoint, 'label': 'benign'})
