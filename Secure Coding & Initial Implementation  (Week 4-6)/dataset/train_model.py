35ed23420db4d0e65904dc09cbbe542c30095d0f
Ahmad <ahmed_aaa1988@yahoo.com>
2025-05-09 00:49:44 +0500

    Update train_model.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
@@ -166,4 +166,6 @@ def train_model(X_train, y_train, X_test, y_test):
     os.makedirs('results', exist_ok=True)
     # Make predictions
     y_pred = model.predict(X_test)
+    # Print classification report
+    print("\nClassification Report:")
     
