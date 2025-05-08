891e535160af0d3ad86bc8162ee4ab2863f63dda
Ahmad <ahmed_aaa1988@yahoo.com>
2025-05-09 00:47:11 +0500

    Update train_model.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
@@ -138,3 +138,6 @@ def train_model(X_train, y_train, X_test, y_test):
             cv=3,
             n_jobs=1,
             verbose=2,
+            error_score='raise',
+            return_train_score=True
+        )
