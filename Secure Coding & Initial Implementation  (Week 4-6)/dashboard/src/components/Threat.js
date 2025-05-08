9d58b21ad0fb5079cbb8ce4f77d16418bd47330b
Umar <umarr.k220@gmail.com>
2025-05-08 20:43:46 +0500

    Update Threat.js

--- Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Threat.js
+++ Secure Coding & Initial Implementation  (Week 4-6)/dashboard/src/components/Threat.js
@@ -2,3 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { io } from 'socket.io-client';
+import '../styles/Profile.css';
+import '../styles/Responsive.css';
+import { jsPDF } from 'jspdf';
+import autoTable from 'jspdf-autotable';
