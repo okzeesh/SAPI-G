b7b0191c4c2a2204e0b66c323f37f21c38a86e4c
AyelaIsrarHaqqani <ayelaisrarhaqqani@gmail.com>
2025-05-09 02:35:16 +0500

    Update generate_best_raw_attack_dataset.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/generate_best_raw_attack_dataset.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/generate_best_raw_attack_dataset.py
@@ -140,3 +140,4 @@ for _ in range(bruteforce_count):
     password = random.choice(bruteforce_passwords)
     user_agent = random.choice(user_agents)
     ip = generate_random_ip()
+    timestamp = random_timestamp()
