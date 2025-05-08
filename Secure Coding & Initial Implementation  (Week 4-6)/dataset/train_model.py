6535cc15269d17af95e470d66d3d1a7d091fd3b4
Ahmad <ahmed_aaa1988@yahoo.com>
2025-05-09 00:43:19 +0500

    Update train_model.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/train_model.py
@@ -110,3 +110,5 @@ def train_model(X_train, y_train, X_test, y_test):
     
     # Create sample weights based on class weights
     sample_weights = np.array([class_weights[label] for label in y_train])
+     # Create base model
+    model = RandomForestClassifier(
