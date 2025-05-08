92e31aafc9df9fc7f59b72228f12e87e667d76aa
AyelaIsrarHaqqani <ayelaisrarhaqqani@gmail.com>
2025-05-09 02:16:10 +0500

    Update generate_best_raw_attack_dataset.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/generate_best_raw_attack_dataset.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/generate_best_raw_attack_dataset.py
@@ -35,4 +35,5 @@ email_domains = ['gmail.com', 'yahoo.com', 'hotmail.com', 'outlook.com', 'proton
 # More diverse user agents
 user_agents = [
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
 
