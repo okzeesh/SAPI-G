577289899f70d38131f6bdb1d0016e0b3ae36db5
AyelaIsrarHaqqani <ayelaisrarhaqqani@gmail.com>
2025-05-09 02:16:29 +0500

    Update generate_best_raw_attack_dataset.py

--- Secure Coding & Initial Implementation  (Week 4-6)/dataset/generate_best_raw_attack_dataset.py
+++ Secure Coding & Initial Implementation  (Week 4-6)/dataset/generate_best_raw_attack_dataset.py
@@ -37,4 +37,5 @@ user_agents = [
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 17_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Mobile/15E148 Safari/604.1',
 
